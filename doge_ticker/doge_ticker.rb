d3e8428b69b19c3c181badedff5fab14ca4140a5
clindsay107 <clindsay107@gmail.com>
2014-01-23 10:33:37 -0800

    added conversion, info and woof commands (much important, such priority)

--- doge_ticker/doge_ticker.rb
+++ doge_ticker/doge_ticker.rb
@@ -25,10 +25,18 @@ class IRCBot
 
 	def parse_line(line)
 		parts = line.split
-		if parts.length == 4
+		if parts.length >= 4
 			msg = parts[3..-1].join(" ")[1..-1]
-			get_price if /^(\!doge)/ =~ msg
-			convert_price(msg.match(/\!c\s(\d+)/)[1]) if /\!c\s(\d+)/ =~ msg
+			say_to_channel "woof" if msg.include?("DogeBot")
+			if /^(\!doge)/ =~ msg
+				get_price
+			elsif /^\!c\s(\d+)/ =~ msg
+				convert_price(Regexp.last_match(1))
+			elsif msg == "!info"
+				say_to_channel 'I am an IRC bot written in ruby to help with DogeCoin related tasks. PM BradPitt with feature requests.'\
+				' I am using data from http://cryptsy.com (for DOGE) and http://vircurex.com (for BTC/USD conversion).'\
+				' Source code available at http://github.com/clindsay107/Doge_ticker'
+			end
 		end
 	end
 
@@ -50,14 +58,34 @@ class IRCBot
 	end
 
 	def convert_price(amount)
-		url = "https://api.bitcoinaverage.com/exchanges/USD"
+
+		#fetch current BTC price
+		url = "http://api.bitcoinaverage.com/exchanges/USD"
 		response = Net::HTTP.get_response(URI(url))
 		begin
 			data = JSON.parse(response.read_body)
-			last_btc_price = data["mtgox"]["rates"]["last"]
+			last_btc_price = data["vircurex"]["rates"]["last"]
 		rescue
-			say_to_channel "Many error, such sorry. Contact head shibe for troubleshooting!"
+			say_to_channel "Many error when fetching BTC price, such sorry. Contact head shibe for troubleshooting!"
+			return
 		end
+
+		#fetch current DOGE price
+		url = "http://pubapi.cryptsy.com/api.php?method=singlemarketdata&marketid=132"
+		response = Net::HTTP.get_response(URI(url))
+		begin 
+			data = JSON.parse(response.read_body)
+			last_doge_price = data["return"]["markets"]["DOGE"]["recenttrades"][0]["price"]
+		rescue JSON::ParserError
+			say_to_channel "Much error, such 502 Bad Gateway when fetching DOGE price. Contact head shibe for troubleshooting!"
+			return
+		end
+
+		puts ">>>BTC: #{last_btc_price}"
+		puts ">>>DOGE: #{last_doge_price}"
+
+		amount_usd = (last_btc_price.to_f * last_doge_price.to_f) * amount.to_f
+		say_to_channel "$\x02#{amount_usd.round(2)}\x02 USD"
 	end
 
 	def run
